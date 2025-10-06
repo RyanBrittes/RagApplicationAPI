517c24b80000dc4797d303a79770c2d05e270913
ryanbrittes <87885748+RyanBrittes@users.noreply.github.com>
2025-10-06 21:00:59 +0000

    [EDIT] - Implementação de código na pasta Test

--- Test/test.py
+++ Test/test.py
@@ -7,10 +7,12 @@ class Test():
         self.pdf_file = fitz.open(self.pdf_path)
         self.chunk_size = 500
         self.overlap_size = 30
-
+        self.token_chunk_size = 50
+        self.token_overlap_size = 10
 
     #Extrai o texto do PDF, armazena em uma string e retorna o valor
     def extract_text(self):
+        text = ""
 
         for page in self.pdf_file:
             text += page.get_text("text")
@@ -41,7 +43,7 @@ class Test():
 
         return [text, tokens]
     
-    def split_data_period(self):
+    def split_data_per_period(self):
         text = self.extract_text_and_token()[0]
         chunks = []
         initial_point = 0
@@ -57,7 +59,7 @@ class Test():
 
         return chunks
             
-    def split_data(self):
+    def split_static_data(self):
         text = self.extract_text_and_token()[0]
         chunks = []
         start = 0
@@ -78,6 +80,43 @@ class Test():
         
         return chunks
 
+    def split_data_per_token(self):
+        tokens = self.extract_text_and_token()[1]
+        chunks = []
+        phrase = ""
+        overlap = ""
+        count = 0
+        count_aux = 0
+        initial_state = 0
+        
+        #Implementar com While ou For, analisar em qual dos dois é possível implementar o overlap
+        while initial_state < len(tokens):
+            phrase += tokens[count]
+            overlap += tokens[self.token_overlap_size-self.token_chunk_size]
+
+            if count == self.token_chunk_size or count_aux == len(tokens):
+                chunks.append(overlap + " " + phrase)
+                count = 0
+                phrase = ""
+
+            count += 1
+
+        for token in tokens:
+            phrase += token
+            count += 1
+            count_aux += 1
+
+            if token == tokens[-self.overlap_size]:
+                overlap += token
+                print(overlap)
+
+            if count == self.token_chunk_size or count_aux == len(tokens):
+                chunks.append(overlap + " " + phrase)
+                count = 0
+                phrase = ""
+        
+        return chunks
+
 A = Test()
 
-print(A.extract_text_and_token()[1])
+print(A.split_data_per_token()[:2])

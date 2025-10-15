8e5e987db4e3282f2799d125a3444fde0f4a0667
ryanbrittes <ryanbrittes027@gmail.com>
2025-10-15 18:36:33 -0400

    [EDIT] - Adicionado CORS para que seja possível controlar quem pode acessar as rotas da API

--- App/Frontend/api.py
+++ App/Frontend/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from chat import Chat
 
 chatFlow = Chat()
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/input', methods=['POST'])
 def add_message():

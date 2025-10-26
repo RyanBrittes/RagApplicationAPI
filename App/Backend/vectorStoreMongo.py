f533f58923cc7025cf3dab4140274e849746510a
ryanbrittes <ryanbrittes027@gmail.com>
2025-10-25 20:03:07 -0400

    [EDIT] - Arquivo vectorStoreMongo.py atualizado para se conectar com o banco de dados Atlas em cloud

--- App/Backend/vectorStoreMongo.py
+++ App/Backend/vectorStoreMongo.py
@@ -1,6 +1,7 @@
 from embedGenerate import EmbedGenerate
 from chunkGenerate import ChunkGenerate
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 
@@ -8,7 +9,7 @@ load_dotenv()
 
 class VectorStoreMongo():
     def __init__(self):
-        self.mongo_client = MongoClient(os.getenv("MONGO_ADDRESS"))
+        self.mongo_client = MongoClient(os.getenv("MONGO_ADDRESS"), server_api=ServerApi('1'))
         self.db_access = self.mongo_client[os.getenv("MONGO_DB")]
         self.collection_access = self.db_access[os.getenv("MONGO_COLLECTION")]
         self.embedding = EmbedGenerate()
@@ -25,7 +26,7 @@ class VectorStoreMongo():
                 'chunk': chunk_collection[i]
             }
 
-        self.collection_access.insert_one(new_object)
+            self.collection_access.insert_one(new_object)
 
     def insert_several(self):
         chunk_collection = self.chunking.create_dinamic_chunk()
@@ -34,3 +35,6 @@ class VectorStoreMongo():
         self.collection_access.insert_many(
             {'_id': i, 'vector': embed_collection[i], 'chunk': chunk_collection[i]} for i in range(len(chunk_collection))
             )
+
+    def ping(self):
+        self.mongo_client.admin.command('ping')
